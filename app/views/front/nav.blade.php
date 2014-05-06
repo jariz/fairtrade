29d632b6e92d20cb3d9cab8833327a7072c9bc38
JariZ <the@jariz.nl>
2014-05-06 15:25:36 +0200

    layout & nav reverted naar 'oude' layout

--- app/views/front/nav.blade.php
+++ app/views/front/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="" role="navigation">
+<nav class="navbar navbar-default" role="navigation">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -8,11 +8,12 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
+      <a class="navbar-brand" href="{{URL::to('/')}}">Fairtrade Amsterdam</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="menu">
-      <ul>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
         @foreach($menuData as $menuItem)
             @if( !isset($menuItem['sub_menu'] ) || count($menuItem['sub_menu']) == 0 )
                 <li @if(Request::is($menuItem['slug'])) class="active" @endif><a href="{{URL::to($menuItem['slug'])}}">{{$menuItem['menu_title']}}</a></li>
@@ -29,16 +30,8 @@
                 </li>
             @endif
         @endforeach
-
-          <!-- TODO HARDCODED VOOR BETA VERSIE!!!!!!!!!! -->
-          <!-- TODO ERUIT HALEN VOOR PROD !!!!!!!!!!??!?!?! -->
-          <li>
-              <a href="{{url('waartekoop')}}">Waar te koop?</a>
-          </li>
-          <li>
-              <a href="{{url('bedrijf-aanmelden')}}">Bedrijf aanmelden</a>
-          </li>
       </ul>
+
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
