3974787f4e792ec62a1a5c9f481fe522ea3e5e13
cxzzy <wies_kueter@hotmail.com>
2014-05-14 13:00:55 +0200

    Company signup form bootstrapped.

--- app/views/front/company/applycompany.blade.php
+++ app/views/front/company/applycompany.blade.php
@@ -10,31 +10,43 @@
 	1: Account aanmaken - <strong>2: Bedrijfsgegevens</strong> - 3: Betalings gegevens
 	<h1>Bedrijf aanmelden</h1>
 	{{ Form::open(array('method' => 'post', 'action' => '\\Front\\Company@add', 'files' => true)) }}
-		{{ Form::label('name', 'Bedrijfsnaam: ') }}
-		{{ Form::text('name') }} <br />
-
-		{{ Form::label('logo', 'Logo: ') }}
-		{{ Form::file('logo') }} <br />
-
-		{{ Form::label('address', 'Straat: ') }}
-		{{ Form::text('address', null, array('class' => '')) }} <br />
-
-		{{ Form::label('postal_code', 'Postcode: ') }}
-		{{ Form::text('postal_code') }} <br />
-
-		{{ Form::label('city', 'Woonplaats: ') }}
-		{{ Form::text('city') }} <br />
-
-		{{ Form::label('description', 'Beschrijving') }} <br />
-		{{ Form::textarea('description') }} <br/ >
-
+        <div class="form-group">
+		    {{ Form::label('name', 'Bedrijfsnaam: ') }}
+		    {{ Form::text('name', NULL, array('class' => 'form-control')) }}
+        </div>
+        <div class="form-group">
+		    {{ Form::label('logo', 'Logo: ') }}
+		    {{ Form::file('logo', NULL, array('class' => 'form-control')) }}
+		</div>
+		<div class="form-group">
+		    {{ Form::label('category', 'Categorie: ') }}
+		     {{ Form::select('category', array('0' => 'Supermarkt', '1' => 'Cadeau winkel', '1' => 'Test categorie'), NULL, array('class' => 'form-control', 'multiple')) }}
+		</div>
+        <div class="form-group">
+		    {{ Form::label('address', 'Straat: ') }}
+		    {{ Form::text('address', NULL, array('class' => 'form-control')) }} <br />
+        </div>
+        <div class="form-group">
+		    {{ Form::label('postal_code', 'Postcode: ') }}
+		    {{ Form::text('postal_code', NULL, array('class' => 'form-control')) }}
+        </div>
+        <div class="form-group">
+		    {{ Form::label('city', 'Woonplaats: ') }}
+		    {{ Form::text('city', NULL, array('class' => 'form-control')) }}
+        </div>
+        <div class="form-group">
+		    {{ Form::label('description', 'Beschrijving') }} <br />
+		    {{ Form::textarea('description', NULL, array('class' => 'form-control')) }}
+        </div>
 		<h2>Contact gegevens</h2>
-		{{ Form::label('contact_info', 'Contact informatie:') }} <br />
-		{{ Form::textarea('contact_info') }} <br />
-
-		{{ Form::label('url', 'Website: ') }}
-		{{ Form::text('url') }} <br />
-
+		<div class="form-group">
+		    {{ Form::label('contact_info', 'Contact informatie:') }} <br />
+		    {{ Form::textarea('contact_info', NULL, array('class' => 'form-control')) }}
+        </div>
+		<div class="form-group">
+		    {{ Form::label('url', 'Website: ') }}
+		    {{ Form::text('url', NULL, array('class' => 'form-control')) }}
+        </div>
 		{{ Form::submit('Bedrijf aanmelden', array('class' => 'btn btn-primary')) }}
 	{{ Form::close() }}
 @stop
