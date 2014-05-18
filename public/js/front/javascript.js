24178bc5ce5a8e31792ec55329659b0c0dcded5e
cxzzy <wies_kueter@hotmail.com>
2014-05-18 13:35:05 +0200

    JavaScript error fixed. API does't use json_encode anymore.

--- public/js/front/javascript.js
+++ public/js/front/javascript.js
@@ -1,46 +1,41 @@
-$(function ()
-{
-	if($('#gmaps').length != 0)
-	{
-	   // Settings
-	    var geocoder = new google.maps.Geocoder();
-	    var address = 'Amsterdam';
-	    //var image = '/proj/travel/usa/imgs/marker.png';
+$(function () {
+    if ($('#gmaps').length != 0) {
+        // Settings
+        var geocoder = new google.maps.Geocoder();
+        var address = 'Amsterdam';
+        //var image = '/proj/travel/usa/imgs/marker.png';
 
-	    geocoder.geocode({
-	        "address": address
-	    }, function (results, status) {
-	        if (status == google.maps.GeocoderStatus.OK) {
-	            map.setCenter(results[0].geometry.location);
-	        }
-	    });
+        geocoder.geocode({
+            "address": address
+        }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+            }
+        });
 
-	    var mapProp = {
-	        zoom: 13,
-	        disableDefaultUI: true,
-	        zoomControl: true,
-	        scrollwheel: false,
-	        draggable: true,
-	        mapTypeId: google.maps.MapTypeId.ROADMAP
-	    };
+        var mapProp = {
+            zoom: 13,
+            disableDefaultUI: true,
+            zoomControl: true,
+            scrollwheel: false,
+            draggable: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
 
-	    var map = new google.maps.Map(document.getElementById("gmaps"), mapProp);
-	    var infowindow = new google.maps.InfoWindow(), marker, i;
+        var map = new google.maps.Map(document.getElementById("gmaps"), mapProp);
+        var infowindow = new google.maps.InfoWindow(),
+            marker, i;
 
-	    google.maps.event.addListener(map, 'zoom_changed', function() 
-	    {
-	        var zoomLevel = map.getZoom();
-	        console.log('Zoom: ' + zoomLevel);
-	    });
+        google.maps.event.addListener(map, 'zoom_changed', function () {
+            var zoomLevel = map.getZoom();
+            console.log('Zoom: ' + zoomLevel);
+        });
 
-	    var geo_location;
+        var geo_location;
 
-	    // Loop through all companies and add them to map
-        $.get( "api/companies?type=locations", function(data) {
-            var obj = jQuery.parseJSON(data);
-
-            $.each(obj, function(key, value)
-            {
+        // Loop through all companies and add them to map
+        $.get("api/companies?type=locations", function (data) {
+            $.each(data, function (key, value) {
                 var marker = new google.maps.Marker({
                     position: new google.maps.LatLng(value.lat, value.lng),
                     map: map,
@@ -51,42 +46,41 @@ $(function ()
             });
         });
 
-	    // Suggestions for a new place
-	    var defaultBounds = new google.maps.LatLngBounds(
-	        new google.maps.LatLng(''),
-	        new google.maps.LatLng()
-	    );
+        // Suggestions for a new place
+        var defaultBounds = new google.maps.LatLngBounds(
+            new google.maps.LatLng(''),
+            new google.maps.LatLng()
+        );
 
-	    var options = {
-	        //bounds: defaultBounds,
-	        //types: ['establishment']
-	    };
+        var options = {
+            //bounds: defaultBounds,
+            //types: ['establishment']
+        };
 
-	    var input = document.getElementById('add_place_input');
+        var input = document.getElementById('add_place_input');
 
-	    // Create the autocomplete object.
-	    var autocomplete = new google.maps.places.Autocomplete(input, options);
-	   
-	    // Move map to searched query
-	    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+        // Create the autocomplete object.
+        var autocomplete = new google.maps.places.Autocomplete(input, options);
 
-	    function onPlaceChanged() 
-	    {
-	        var place = autocomplete.getPlace();
-	        if (place.geometry) {
-	            map.panTo(place.geometry.location);
-	            map.setZoom(15);
-	            
-	            var marker = new google.maps.Marker({
-	                position: place.geometry.location,
-	                map: map,
-	                title: 'Winkel naam'
-	            });
+        // Move map to searched query
+        google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
 
-	            search();
-	        } else {
-	            document.getElementById('autocomplete').placeholder = 'Enter a city';
-	        }
-	    }		
-	}
+        function onPlaceChanged() {
+            var place = autocomplete.getPlace();
+            if (place.geometry) {
+                map.panTo(place.geometry.location);
+                map.setZoom(15);
+
+                var marker = new google.maps.Marker({
+                    position: place.geometry.location,
+                    map: map,
+                    title: 'Winkel naam'
+                });
+
+                search();
+            } else {
+                document.getElementById('autocomplete').placeholder = 'Enter a city';
+            }
+        }
+    }
 });
