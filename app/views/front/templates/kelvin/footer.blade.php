1e144cf031c5fe0c44e434c7a477434af1c83148
JariZ <spam@jariz.pro>
2014-05-20 00:30:18 +0200

    Dashboard url fixed

--- app/views/front/templates/kelvin/footer.blade.php
+++ app/views/front/templates/kelvin/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="border_horizontal"></div>
     <ul class="list-inline voorwaarden">
         <li><a href="">Voorwaarden</a></li>
-        <li><a href="{{URL::route('">Beheer</a></li>
+        <li><a href="{{URL::route('dashboard')}}">Beheer</a></li>
     </ul>
     <ul class="list-inline social">
         <li class="fb"><a href=""><i class="fa fa-facebook-square"></i></a></li>
