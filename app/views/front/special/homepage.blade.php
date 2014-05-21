b117b5a5c6d0e0df3aa8da2be6cece6022b83f4b
JariZ <the@jariz.nl>
2014-05-21 11:57:46 +0200

    Homepage formatted

--- app/views/front/special/homepage.blade.php
+++ app/views/front/special/homepage.blade.php
@@ -5,19 +5,24 @@
     <div class="row sliderWrap">
         <div class="col-md-12 slider"><img src="images/slider_visual.jpg" alt=""></div>
         <div class="col-md-12 slideOverlay"><img src="images/slider_overlay.png" alt="">
-          <div class="video">
-            <h1>Ben jij al Fairtrade?</h1>
-            <div class="video-container">
-            <iframe width="375" height="211"
-                    src="//www.youtube.com/embed/OsRFTWLiP9g?$list=PL2zniuqMzqkaURTd0uhm9iZqtBDkY7n5Y" frameborder="0"
-                    allowfullscreen></iframe>
+
+            <div class="video">
+                <h1>Ben jij al Fairtrade?</h1>
+
+                <div class="video-container">
+                    <iframe width="375" height="211"
+                            src="//www.youtube.com/embed/OsRFTWLiP9g?$list=PL2zniuqMzqkaURTd0uhm9iZqtBDkY7n5Y"
+                            frameborder="0"
+                            allowfullscreen></iframe>
+                </div>
+
+                <div class="partner"><a href="{{ URL::to('bedrijf-aanmelden') }}">Word nu een partner</a></div>
             </div>
-
-            <div class="partner"><a href="{{ URL::to('bedrijf-aanmelden') }}">Word nu een partner</a></div>
-        </div></div>
+        </div>
 
 
-    </div><div class="clear"></div>
+    </div>
+    <div class="clear"></div>
 </div>
 
 <div class="row">
@@ -50,7 +55,7 @@
     </div>
 </div>
     <div class="col-md-12 visible-md visible-sm visible-xs">
-           <div class="border_horizontal"></div>
+        <div class="border_horizontal"></div>
     </div>
     <div class="col-md-8">
         <div class="border_vertical homeVertical hidden-md hidden-sm hidden-xs"></div>
@@ -67,26 +72,27 @@
                            <img src="images/h2Right.png" alt="">
                        </span>
                    </span>
+
             <div>
-            <img
-                src="{{$featured->company->logo ? url('uploads/logos/'.$featured->company->logo) : 'http://placehold.it/220x230'}}"
-                alt="" class="pull-left giev-padding"/>
+                <img
+                    src="{{$featured->company->logo ? url('uploads/logos/'.$featured->company->logo) : 'http://placehold.it/220x230'}}"
+                    alt="" class="pull-left giev-padding"/>
 
 
-            <h3>{{{$featured->company->name}}}</h3>
-            <h4>{{{$featured->company->city}}}</h4>
-            <h4>{{{$featured->company->adres}}}</h4>
-            {{$featured->content}}
+                <h3>{{{$featured->company->name}}}</h3>
+                <h4>{{{$featured->company->city}}}</h4>
+                <h4>{{{$featured->company->adres}}}</h4>
+                {{$featured->content}}
             </div>
-        <div class="clear"></div>
-        <div class="border_horizontal"></div>
+            <div class="clear"></div>
+            <div class="border_horizontal"></div>
 
         </div>
 
-<!--        @if($featured->company->website)-->
-<!--        <div class="link"><a href="{{{$featured->company->website}}}">Ga naar de website</a></div>-->
-<!--        @endif-->
- <div class="uitgelicht uitgelichtHome col-md-12">
+        <!--        @if($featured->company->website)-->
+        <!--        <div class="link"><a href="{{{$featured->company->website}}}">Ga naar de website</a></div>-->
+        <!--        @endif-->
+        <div class="uitgelicht uitgelichtHome col-md-12">
                    <span class="h2Wrap">
                        <span class="h2Left">
                            <img src="images/h2Left.png" alt="">
@@ -98,66 +104,66 @@
                            <img src="images/h2Right.png" alt="">
                        </span>
                    </span>
-                    </span>
-                        <ul class="list-inline">
-                            <li class="first-child">
-                                <ul>
-                                    <li class="image">
-                                        <img src="http://placehold.it/158x106" alt="">
-                                    </li>
-                                    <li>
-                                        <h2>Dinner & co</h2>
-                                    </li>
-                                    <li>
-                                        <h3>Fairtrade wijn cursus</h3>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul>
-                                    <li>
-                                        <img src="http://placehold.it/158x106" alt="">
-                                    </li>
-                                    <li>
-                                        <h2>Dinner & co</h2>
-                                    </li>
-                                    <li>
-                                        <h3>Fairtrade wijn cursus</h3>
-                                    </li>
-                                </ul>
-                            </li>
-                      
-                            <li>
-                                <ul>
-                                    <li class="image">
-                                        <img src="http://placehold.it/158x106" alt="">
-                                    </li>
-                                    <li>
-                                        <h2>Dinner & co</h2>
-                                    </li>
-                                    <li>
-                                        <h3>Fairtrade wijn cursus</h3>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <ul>
-                                    <li>
-                                        <img src="http://placehold.it/158x106" alt="">
-                                    </li>
-                                    <li>
-                                        <h2>Dinner & co</h2>
-                                    </li>
-                                    <li>
-                                        <h3>Fairtrade wijn cursus</h3>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                  <div class="border_horizontal"></div>
-             </div>
-             
-               <div class="clear"></div> 
+            </span>
+            <ul class="list-inline">
+                <li class="first-child">
+                    <ul>
+                        <li class="image">
+                            <img src="http://placehold.it/158x106" alt="">
+                        </li>
+                        <li>
+                            <h2>Dinner & co</h2>
+                        </li>
+                        <li>
+                            <h3>Fairtrade wijn cursus</h3>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <ul>
+                        <li>
+                            <img src="http://placehold.it/158x106" alt="">
+                        </li>
+                        <li>
+                            <h2>Dinner & co</h2>
+                        </li>
+                        <li>
+                            <h3>Fairtrade wijn cursus</h3>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <ul>
+                        <li class="image">
+                            <img src="http://placehold.it/158x106" alt="">
+                        </li>
+                        <li>
+                            <h2>Dinner & co</h2>
+                        </li>
+                        <li>
+                            <h3>Fairtrade wijn cursus</h3>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <ul>
+                        <li>
+                            <img src="http://placehold.it/158x106" alt="">
+                        </li>
+                        <li>
+                            <h2>Dinner & co</h2>
+                        </li>
+                        <li>
+                            <h3>Fairtrade wijn cursus</h3>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <div class="border_horizontal"></div>
+        </div>
+
+        <div class="clear"></div>
          <span class="h2Wrap">
                        <span class="h2Left">
                            <img src="images/h2Left.png" alt="">
@@ -169,9 +175,11 @@
                            <img src="images/h2Right.png" alt="">
                        </span>
                    </span>
+
         <div>
             @foreach($companies as $company)
-            <a href="{{url('waartekoop/bedrijf/'.$company->id)}}"><img src="{{URL::asset('uploads/logos/'.$company->logo)}}" alt="" class="floatLeft"/></a>
+            <a href="{{url('waartekoop/bedrijf/'.$company->id)}}"><img
+                    src="{{URL::asset('uploads/logos/'.$company->logo)}}" alt="" class="floatLeft"/></a>
             @endforeach
         </div>
         @endif
