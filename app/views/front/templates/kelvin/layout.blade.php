925ceac8b6ef59bd265ec0cca69fbfcee42bf9f8
JariZ <spam@jariz.pro>
2014-05-22 21:02:29 +0200

    SEO

--- app/views/front/templates/kelvin/layout.blade.php
+++ app/views/front/templates/kelvin/layout.blade.php
@@ -4,13 +4,23 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{$title or "GEEN TITEL"}} - Fairtrade Amsterdam</title>
-    <meta name="description" content="">
+    <meta name="keywords" content="fairtrade,eerlijk,fair,amsterdam,fairtrade gemeente,koffie,muncipality">
+    <meta name="description" content="Fairtrade Amsterdam gaat Amsterdam Fairtrade maken, doet u mee?">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{url('css/front/main.css')}}">
     <link rel="stylesheet" href="{{url('css/front/pages.css')}}">
     <link rel="stylesheet" href="{{url('plugins/bs/bootstrap.front.min.css')}}">
     <link href="{{url('plugins/font-awesome-4.0.3/css/font-awesome.min.css')}}" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Open graph metadata -->
+    <meta property="og:title" content="{{$title or 'Fairtrade Amsterdam'}}"/>
+    <meta property="og:url" content="http://fairtradeamsterdam.nl/"/>
+    <meta property="og:image" content="{{url('images/logo.png')}}"/>
+    <meta property="og:site_name" content="Fairtrade Amsterdam"/>
+    <meta property="og:description" content="Fairtrade Amsterdam gaat Amsterdam Fairtrade maken, doet u mee?"/>
+    <meta property="og:type" content="website"/>
+
     <script src="{{url('plugins/jquery/jquery.min.js')}}"></script>
     <script src="{{url('plugins/bs/bootstrap.min.js')}}"></script>
     <script>
