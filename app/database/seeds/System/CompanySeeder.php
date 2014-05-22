d3a96a5e4f64b667e59b0962f51926201aa21f9e
ssshenkie <crashbandie@hotmail.com>
2014-05-22 11:18:27 +0200

    Auto thumbnail in company seeder FIXED (AGAIN damn)

--- app/database/seeds/System/CompanySeeder.php
+++ app/database/seeds/System/CompanySeeder.php
@@ -227,12 +227,12 @@ class CompanySeeder extends \Seeder
                 $modelCompany->$column = $value;
 
                if($column === 'logo'){
-                   $img = Image::make( public_path().'images/logos/'.$value );
+                   $img = Image::make( public_path().'/images/logos/'.$value )
                     // Save full size
-                     ->save( public_path().'uploads/logos/'.$value;
+                     ->save( public_path().'/uploads/logos/'.$value);
                    // Create thumbnail
                    $img->resize(300, 300)
-                        ->save( public_path().'uploads/logos/t/'.$value);
+                        ->save( public_path().'/uploads/logos/t/'.$value);
                }
            }
 
