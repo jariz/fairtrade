1c0cc56b35d82a60a3aaca039c7162d8537d39fd
JariZ <the@jariz.nl>
2014-05-23 11:12:20 +0200

    Fixed #67

---
+++ app/database/migrations/2014_05_23_090105_news_varchar_to_text.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NewsVarcharToText extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table("news", function(Blueprint $blueprint) {
+            $blueprint->dropColumn("content");
+            $blueprint->text("content");
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::table("news", function(Blueprint $blueprint) {
+            $blueprint->dropColumn("content");
+            $blueprint->string("content");
+        });
+	}
+
+}
