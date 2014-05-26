5081d59891a5ed6d9dfb36d932e68cc6236d3857
ssshenkie <crashbandie@hotmail.com>
2014-05-27 00:07:22 +0200

    Page create error fixed where not using the show_in_nav and published options

--- app/models/Model/Page.php
+++ app/models/Model/Page.php
@@ -8,6 +8,17 @@ class Page extends FormattedTimestamps {
 	public $timestamps = true;
     protected $softDelete = true;
 
+    public static function boot(){
+        static::saving( function( $page ){
+            if( is_null($page->show_in_nav)){
+                $page->show_in_nav = 0;
+            }
+
+            if( is_null($page->published)){
+                $page->published = 0;
+            }
+        } );
+    }
 
 	public function scopeWhereSlug($query, $slug){
 		return $query->where('slug', '=', $slug)->first();
